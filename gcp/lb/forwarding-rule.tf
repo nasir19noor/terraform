429780200a801553e1ab7cf828422cb68d531747
Nasir Noor <nasir@nasir.id>
2023-07-12 14:02:27 +0700

    20230712

--- gcp/lb/forwarding-rule.tf
+++ gcp/lb/forwarding-rule.tf
@@ -6,15 +6,14 @@ resource "google_compute_global_forwarding_rule" "jenkins" {
   load_balancing_scheme = "EXTERNAL"
   port_range            = "80"
   target                = google_compute_target_http_proxy.jenkins.id
-  ip_address = google_compute_address.jenkins-fe.address
+  ///ip_address = google_compute_address.jenkins-fe-2.address
   //network               = google_compute_network.vpc_network.id
   //subnetwork            = google_compute_subnetwork.subnet-sg.id
   //network_tier          = "PREMIUM"
 }
 
 
-resource "google_compute_address" "jenkins-fe" {
-  name         = "jenkins-fe"
-  address_type = "EXTERNAL"
-  region       = "asia-southeast1"
-}
+//resource "google_compute_address" "jenkins-fe-2" {
+//  address_type = "EXTERNAL"
+ // region       = "asia-southeast1"
+//}
