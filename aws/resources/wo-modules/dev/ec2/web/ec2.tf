901e6cbc4797410d600069c4206d06c8ee723529
Nasir Nooruddin <nasir@nasir.id>
2024-01-10 22:08:10 +0700

    update

--- aws/resources/wo-modules/dev/ec2/web/ec2.tf
+++ aws/resources/wo-modules/dev/ec2/web/ec2.tf
@@ -10,11 +10,12 @@ resource "aws_key_pair" "dev_web" {
 }
 
 resource "aws_instance" "dev_web" {
-  ami           = var.ami-amazon[0]
+  ami           = var.ami-ubuntu[0]
   instance_type = var.instance_type[0]
   subnet_id     = data.terraform_remote_state.vpc.outputs.public_subnets[1]
   key_name = aws_key_pair.dev_web.key_name
   vpc_security_group_ids = [aws_security_group.dev_web.id]
+  user_data = file("userdata.tpl")
   tags = {
    Name = "${var.project}-dev-web"
  }
--- aws/resources/wo-modules/dev/ec2/web/outputs.tf
+++ aws/resources/wo-modules/dev/ec2/web/outputs.tf
@@ -2,3 +2,7 @@ output "private_key" {
   value     = tls_private_key.dev_web.private_key_pem
   sensitive = true
 }
+
+output "security_group" {
+  value     = aws_security_group.dev_web.id
+}
