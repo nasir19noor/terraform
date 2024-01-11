d86bf2e27ff5d1ec1b76b3a13decb9ef85ef1285
Nasir Nooruddin <nasir@nasir.id>
2024-01-11 14:41:11 +0700

    update

---
+++ gcp/resources/common/backend/main.tf
@@ -0,0 +1,8 @@
+resource "google_storage_bucket" "backend" {
+ project       = var.project_id
+ name          = "nasir-terraform-state-wo-modules"
+ location      = "ASIA"
+ storage_class = "STANDARD"
+
+ uniform_bucket_level_access = true
+}
