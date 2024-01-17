77912fad2cd6eb963c78ff4001dc485bf1aca4ee
nasir19noor <nasir@nasir.id>
2024-01-17 22:24:53 +0700

    update

--- aws/resources/wo-modules/dev/ec2/backend/ec2.tf
+++ aws/resources/wo-modules/dev/ec2/backend/ec2.tf
@@ -1,23 +1,23 @@
 
-resource "tls_private_key" "dev_web" {
+resource "tls_private_key" "dev_be" {
   algorithm = "RSA"
   rsa_bits = 4096
 }
 
-resource "aws_key_pair" "dev_web" {
+resource "aws_key_pair" "dev_be" {
   key_name = var.key_name
-  public_key = tls_private_key.dev_web.public_key_openssh
+  public_key = tls_private_key.dev_be.public_key_openssh
 }
 
-resource "aws_instance" "dev_web" {
+resource "aws_instance" "dev_be" {
   ami           = var.ami-ubuntu[0]
   instance_type = var.instance_type[0]
-  subnet_id     = data.terraform_remote_state.vpc.outputs.public_subnets[1]
-  key_name = aws_key_pair.dev_web.key_name
-  vpc_security_group_ids = [aws_security_group.dev_web.id]
+  subnet_id     = data.terraform_remote_state.vpc.outputs.private_subnets[1]
+  key_name = aws_key_pair.dev_be.key_name
+  vpc_security_group_ids = [aws_security_group.dev_be.id]
   user_data = file("userdata.tpl")
   tags = {
-   Name = "${var.project}-dev-web"
+   Name = "${var.project}-dev-be"
  }
 }
 
