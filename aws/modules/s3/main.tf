daa32c91e0fef18d980a93598d622a72c2ce34ad
Nasir Noor <nasir@nasir.id>
2025-05-31 11:42:25 +0700

    add public access policy

--- aws/modules/s3/main.tf
+++ aws/modules/s3/main.tf
@@ -6,3 +6,13 @@ resource "aws_s3_bucket" "this" {
   object_lock_enabled = var.object_lock_enabled
   tags                = var.tags
 }
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
+  ignore_public_acls      = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
+}
+
--- aws/modules/s3/variables.tf
+++ aws/modules/s3/variables.tf
@@ -27,3 +27,28 @@ variable "tags" {
   type        = map(string)
   default     = {}
 }
+
+# Public access block variables
+variable "block_public_acls" {
+  description = "Whether Amazon S3 should block public ACLs for this bucket"
+  type        = bool
+  default     = true
+}
+
+variable "block_public_policy" {
+  description = "Whether Amazon S3 should block public bucket policies for this bucket"
+  type        = bool
+  default     = true
+}
+
+variable "ignore_public_acls" {
+  description = "Whether Amazon S3 should ignore public ACLs for this bucket"
+  type        = bool
+  default     = true
+}
+
+variable "restrict_public_buckets" {
+  description = "Whether Amazon S3 should restrict public bucket policies for this bucket"
+  type        = bool
+  default     = true
+}
