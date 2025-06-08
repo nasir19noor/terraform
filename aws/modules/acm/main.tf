61be81b9c3605e11168ab1ce6e45900b42131139
Nasir Noor <nasir@nasir.id>
2025-06-08 11:10:35 +0700

    acm module

--- aws/modules/acm/main.tf
+++ aws/modules/acm/main.tf
@@ -2,9 +2,6 @@ resource "aws_acm_certificate" "this" {
   domain_name          = var.domain_name
   validation_method    = var.validation_method
   wait_for_validation  = var.wait_for_validation
-  lifecycle {
-    create_before_destroy = var.create_before_destroy
-  }
 
   tags = {
     Name = var.domain_name
