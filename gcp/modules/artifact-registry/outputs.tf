b4b3c1f6982efb09ffe78810fe84c359153eca85
Nasir Noor <nasir@nasir.id>
2025-06-21 22:27:08 +0700

    gar

--- gcp/modules/artifact-registry/outputs.tf
+++ gcp/modules/artifact-registry/outputs.tf
@@ -1,49 +1,23 @@
+# Outputs the full name of the created Artifact Registry repository.
+output "repository_name" {
+  description = "The name of the Artifact Registry repository."
+  value       = google_artifact_registry_repository.this.name
+}
+
+# Outputs the location of the created repository.
+output "repository_location" {
+  description = "The location of the Artifact Registry repository."
+  value       = google_artifact_registry_repository.this.location
+}
+
+# Outputs the repository ID.
 output "repository_id" {
-  description = "The ID of the created Artifact Registry repository"
-  value       = google_artifact_registry_repository.repository.repository_id
+  description = "The ID of the Artifact Registry repository."
+  value       = google_artifact_registry_repository.this.repository_id
 }
 
-output "name" {
-  description = "The name of the repository in the format projects/{project}/locations/{location}/repositories/{repository_id}"
-  value       = google_artifact_registry_repository.repository.name
-}
-
-output "location" {
-  description = "The location where the repository is hosted"
-  value       = google_artifact_registry_repository.repository.location
-}
-
-output "format" {
-  description = "The format of packages that are stored in the repository"
-  value       = google_artifact_registry_repository.repository.format
-}
-
-output "description" {
-  description = "The description of the repository"
-  value       = google_artifact_registry_repository.repository.description
-}
-
-output "create_time" {
-  description = "The time when the repository was created"
-  value       = google_artifact_registry_repository.repository.create_time
-}
-
-output "update_time" {
-  description = "The time when the repository was last updated"
-  value       = google_artifact_registry_repository.repository.update_time
-}
-
-output "project" {
-  description = "The project ID where the repository is located"
-  value       = google_artifact_registry_repository.repository.project
-}
-
-output "registry_url" {
-  description = "The URL that can be used to access the repository"
-  value       = "${google_artifact_registry_repository.repository.location}-docker.pkg.dev/${google_artifact_registry_repository.repository.project}/${google_artifact_registry_repository.repository.repository_id}"
-}
-
-output "labels" {
-  description = "Labels applied to the repository"
-  value       = google_artifact_registry_repository.repository.labels
+# Outputs the unique identifier of the repository.
+output "repository_full_id" {
+  description = "The full identifier of the Artifact Registry repository."
+  value       = google_artifact_registry_repository.this.id
 }
--- gcp/modules/artifact-registry/variables.tf
+++ gcp/modules/artifact-registry/variables.tf
@@ -1,97 +1,40 @@
+# Defines the Google Cloud Project ID where the resources will be created.
 variable "project_id" {
-  description = "The GCP project ID where the Artifact Registry repository will be created"
+  description = "The ID of the Google Cloud project."
   type        = string
 }
 
-variable "location" {
-  description = "The location/region where the repository will be created"
+# Defines the name for the Artifact Registry repository.
+variable "name" {
+  description = "The name of the Artifact Registry repository."
+  type        = string
+  default     = "my-artifact-repo"
+}
+
+# Defines the GCP region where the repository will be located.
+variable "region" {
+  description = "The Google Cloud region to deploy the repository to."
   type        = string
   default     = "us-central1"
-  
-  validation {
-    condition = can(regex("^[a-z0-9-]+$", var.location))
-    error_message = "Location must be a valid GCP region or zone name."
-  }
 }
 
-variable "repository_id" {
-  description = "The repository ID (name) for the Artifact Registry repository"
-  type        = string
-  
-  validation {
-    condition     = can(regex("^[a-z0-9-]+$", var.repository_id)) && length(var.repository_id) >= 1 && length(var.repository_id) <= 63
-    error_message = "Repository ID must be 1-63 characters, contain only lowercase letters, numbers, and hyphens."
-  }
-}
-
+# Defines the description for the repository.
 variable "description" {
-  description = "Description of the repository"
+  description = "A brief description for the Artifact Registry repository."
   type        = string
-  default     = "Artifact Registry repository managed by Terraform"
+  default     = "My Awesome Artifact Registry repository"
 }
 
+# Defines the format of the artifacts that the repository will store.
+# Includes a validation block to ensure only supported formats are used.
 variable "format" {
-  description = "The format of packages that are stored in the repository"
+  description = "The format of the repository (e.g., DOCKER, MAVEN, NPM, PYTHON)."
   type        = string
   default     = "DOCKER"
-  
+
   validation {
-    condition = contains([
-      "DOCKER",
-      "MAVEN",
-      "NPM",
-      "PYTHON",
-      "APT",
-      "YUM",
-      "GOOGET",
-      "GO",
-      "KFP",
-      "GENERIC"
-    ], var.format)
-    error_message = "Format must be one of: DOCKER, MAVEN, NPM, PYTHON, APT, YUM, GOOGET, GO, KFP, GENERIC."
+    condition     = contains(["DOCKER", "MAVEN", "NPM", "PYTHON", "APT", "YUM", "GO", "KFP"], upper(var.format))
+    error_message = "The format must be one of the following: DOCKER, MAVEN, NPM, PYTHON, APT, YUM, GO, KFP."
   }
 }
 
-variable "labels" {
-  description = "Labels to be applied to the repository"
-  type        = map(string)
-  default     = null
-}
-
-variable "cleanup_policies" {
-  description = "List of cleanup policies for the repository"
-  type = list(object({
-    id     = string
-    action = string
-    condition = optional(object({
-      tag_state             = optional(string)
-      tag_prefixes          = optional(list(string))
-      version_name_prefixes = optional(list(string))
-      package_name_prefixes = optional(list(string))
-      older_than           = optional(string)
-      newer_than           = optional(string)
-    }))
-    most_recent_versions = optional(object({
-      package_name_prefixes = optional(list(string))
-      keep_count           = optional(number)
-    }))
-  }))
-  default = []
-}
-
-variable "docker_config" {
-  description = "Docker-specific configuration for the repository (only applicable when format is DOCKER)"
-  type = object({
-    immutable_tags = optional(bool)
-  })
-  default = null
-}
-
-variable "maven_config" {
-  description = "Maven-specific configuration for the repository (only applicable when format is MAVEN)"
-  type = object({
-    allow_snapshot_overwrites = optional(bool)
-    version_policy           = optional(string)
-  })
-  default = null
-}
